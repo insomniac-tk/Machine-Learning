2e71ab8fce418c6af470bb7a6f4ca5a2796006f4
insomniac-tk <shirleyhouse20@gmail.com>
2018-10-14 22:44:15 +0530

    FIRST COMMIT AFTER 4 MONTHS!

---
+++ clustering_helloworld.py
@@ -0,0 +1,7 @@
+"""
+K-Means falls under the category of centroid-based clustering. A centroid is a data point (imaginary or real) at the center of a cluster. In centroid-based clustering, clusters are represented by a central vector or a centroid. 
+This centroid might not necessarily be a member of the dataset.
+Centroid-based clustering is an iterative algorithm in which the notion of similarity is derived by how close a data point is to the centroid of the cluster.
+"""
+
+print("A basic example of centroid based clustering!")
